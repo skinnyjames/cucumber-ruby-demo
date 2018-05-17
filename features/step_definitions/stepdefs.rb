46147cf2e3cbe19941d5d894d8700b3ca973fdd4
Sean Gregory <seangregory@protonmail.com>
2018-05-17 17:11:04 -0500

    fix tabs

--- features/step_definitions/stepdefs.rb
+++ features/step_definitions/stepdefs.rb
@@ -1,27 +1,27 @@
 require File.dirname(__FILE__) + '/../../src/search-client.rb'
 
 Given("the user searches google") do
-	begin 
-		@results = SearchClient.search(%w{cucumber ruby google})
-	rescue SearchClientError => e
-		@error = e
-	end
+  begin 
+    @results = SearchClient.search(%w{cucumber ruby google})
+  rescue SearchClientError => e
+    @error = e
+  end
 end
 
 When("the search is successful") do
-	expect(@error.nil?).to be true
+  expect(@error.nil?).to be true
 end
 
 Then("the user should see links") do
-	expect(@results.length).to be > 0
+  expect(@results.length).to be > 0
 end
 
 Given("the user searches google with no input") do 
-	begin 
-		@results = SearchClient.search([])
-	rescue SearchClientError => e
-		@error = e
-	end
+  begin 
+    @results = SearchClient.search([])
+  rescue SearchClientError => e
+    @error = e
+  end
 end
 
 When("the search is unsuccessful") do 
